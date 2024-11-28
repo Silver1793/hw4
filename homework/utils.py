769f8fd72cc363b0f348187558ecc54ab6de251d
Richard Li <richardli@richards-mbp.myfiosgateway.com>
2024-11-28 11:56:06 -0500

    x

--- homework/utils.py
+++ homework/utils.py
@@ -1,31 +1,23 @@
-import csv
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import ToTensor
+from PIL import Image
 from pathlib import Path
 
-import torch
-from PIL import Image
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
-
-LABEL_NAMES = ["background", "kart", "pickup", "nitro", "bomb", "projectile"]
-
+LABEL_NAMES = ['label1', 'label2', 'label3']  # Replace with your actual label names
 
 class SuperTuxDataset(Dataset):
     def __init__(self, dataset_path: str):
-        """
-        Pairs of images and labels (int) for classification
-        You won't need to modify this, but all PyTorch datasets must implement these methods
-        """
-        to_tensor = transforms.ToTensor()
-
         self.data = []
+        self.dataset_path = dataset_path
+        self.to_tensor = ToTensor()
 
-        with open(Path(dataset_path, "labels.csv"), newline="") as f:
-            for fname, label, _ in csv.reader(f):
-                if label in LABEL_NAMES:
-                    image = Image.open(Path(dataset_path, fname))
-                    label_id = LABEL_NAMES.index(label)
-
-                    self.data.append((to_tensor(image), label_id))
+        for fname in Path(dataset_path).glob('*.png'):  # Assuming images are in .png format
+            label = fname.stem.split('_')[-1]  # Assuming label is part of the filename
+            if label in LABEL_NAMES:
+                image = Image.open(fname)
+                label_id = LABEL_NAMES.index(label)
+                self.data.append((self.to_tensor(image), label_id))
 
     def __len__(self):
         return len(self.data)
@@ -36,7 +28,6 @@ class SuperTuxDataset(Dataset):
 
 def load_data(dataset_path: str, num_workers: int = 0, batch_size: int = 128, shuffle: bool = False) -> DataLoader:
     dataset = SuperTuxDataset(dataset_path)
-
     return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=shuffle, drop_last=True)
 
 
@@ -49,6 +40,7 @@ def compute_accuracy(outputs: torch.Tensor, labels: torch.Tensor):
     Returns:
         a single torch.Tensor scalar
     """
-    outputs_idx = outputs.max(1)[1].type_as(labels)
-
-    return (outputs_idx == labels).float().mean()
+    _, predicted = torch.max(outputs, 1)
+    correct = (predicted == labels).sum().item()
+    accuracy = correct / labels.size(0)
+    return torch.tensor(accuracy)
